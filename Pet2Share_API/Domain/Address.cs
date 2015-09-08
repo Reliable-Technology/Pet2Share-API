0640b09cf50b8c8aeb9a8872ec92a49c9890b5de
shishir333 <shishir333@gmail.com>
2015-09-08 11:48:52 -0700

    Initial commit

---
+++ Pet2Share_API/Domain/Address.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet2Share_API.Domain
+{
+    class Address
+    {
+        #region members
+        public int Id { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        #endregion
+
+        #region constructors
+        public Address()
+        {
+
+        }
+
+        public Address(int id)
+        {
+
+        }
+
+        public Address(string addressLine1, string addressLine2, string city, string state, string country)
+        {
+            
+        }
+        #endregion
+
+        #region methods
+
+        public string Save()
+        {
+            return "";
+        }
+
+        #endregion
+    }
+}
---
+++ Pet2Share_API/Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet2Share_API.Domain
+{
+    class User
+    {
+        public string Username { get; set; }
+        public bool IsAuthenticated { get; set; }
+        
+    }
+}
