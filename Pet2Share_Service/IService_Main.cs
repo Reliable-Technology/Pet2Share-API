30ff685eb95be24f0e1b71f0db765a7dd642d7d6
Devteam1 <devteam1@dynamicsonline.co.uk>
2015-09-23 12:44:37 -0500

    Updates for following the right naming conventions

--- Pet2Share_Service/IService_Main.cs
+++ Pet2Share_Service/IService_Main.cs
@@ -10,45 +10,32 @@ using System.Text;
 
 namespace Pet2Share_Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService_Main
     {
 
-        #region Commented
-        //[OperationContract]
-        //string GetData(int value);
-
-        //[OperationContract]
-        //CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        //[OperationContract]
-        //string LoginUser(string Username, string Password); 
-        #endregion
 
         [OperationContract]
         [WebInvoke(UriTemplate = "LoginUser", Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        Message LoginUser(CLLogin LoginDetails);
+        Message LoginUser(LoginCL loginDetails);
 
-
-        // TODO: Add your service operations here
     }
 
     [DataContract]
     public class ResponseCL
     {
         [DataMember]
-        public int total { get; set; }
+        public int Total { get; set; }
         [DataMember]
-        public object[] results { get; set; }
+        public object[] Results { get; set; }
         [DataMember]
-        public ErrorMessageCL errorMessage { get; set; }
+        public ErrorMessageCL ErrorMessage { get; set; }
 
-        public ResponseCL(int Total, object[] Results, ErrorMessageCL ErrMsg)
+        public ResponseCL(int total, object[] results, ErrorMessageCL errMsg)
         {
-            total = Total;
-            results = Results;
-            errorMessage = ErrMsg;
+            Total = total;
+            Results = results;
+            ErrorMessage = errMsg;
 
         }
 
@@ -58,22 +45,22 @@ namespace Pet2Share_Service
     public class ErrorMessageCL
     {
         [DataMember]
-        public int reasonCode { get; set; }
+        public int ReasonCode { get; set; }
 
         [DataMember]
-        public string message { get; set; }
+        public string Msg { get; set; }
 
-        public ErrorMessageCL(int RsnCode, string Msg)
+        public ErrorMessageCL(int rsnCode, string msg)
         {
-            reasonCode = RsnCode;
-            message = Msg;
+            ReasonCode = rsnCode;
+            Msg = msg;
         }
 
 
     }
 
 
-    public class CLLogin
+    public class LoginCL
     {
 
         public string UserName { get; set; }
@@ -92,47 +79,5 @@ namespace Pet2Share_Service
         Failed,
         Exception
     }
-
-
-    [DataContract]
-    public class stringMessage
-    {
-        [DataMember]
-        public string Message { get; set; }
-
-        [DataMember]
-        public ResponseType Resp { get; set; }
-
-        public stringMessage(string _Message, ResponseType _Resp)
-        {
-            Message = _Message;
-            Resp = _Resp;
-        }
-    }
-
-
-    #region Commented
-    //// Use a data contract as illustrated in the sample below to add composite types to service operations.
-    //[DataContract]
-    //public class CompositeType
-    //{
-    //    bool boolValue = true;
-    //    string stringValue = "Hello ";
-
-    //    [DataMember]
-    //    public bool BoolValue
-    //    {
-    //        get { return boolValue; }
-    //        set { boolValue = value; }
-    //    }
-
-    //    [DataMember]
-    //    public string StringValue
-    //    {
-    //        get { return stringValue; }
-    //        set { stringValue = value; }
-    //    }
-    //} 
-    #endregion
 
 }
