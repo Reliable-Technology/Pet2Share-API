91e2740021f7bdf72d0f4daa85f2aed0b29daea2
shishir thakur <shishir333@gmail.com>
2015-10-21 11:29:13 -0400

    Updated dlls and all other files

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Pet2Share_API.Domain;
+using Pet2Share_API.Service;
+using Pet2Share_API.Utility;
+using System.IO;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Post> postList = PostManager.GetPostsByUser(9, 2, 2);
+
+            AccountManagement.Login("nish@gmail.com", "test");
+            //Post post = new Post("This is a test post", 1, true, 1);
+
+            //PostManager.AddPost(post);
+            //PostManager.AddComment(post.Id, 9, false, "Test comment");
+
+            //post.Comments = PostManager.GetComments(post.Id);
+
+            FileStream fs = File.Open(@"C:\Users\PS\Desktop\test\banner.jpg", FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            
+            byte[] imageArr = br.ReadBytes(Convert.ToInt32(br.BaseStream.Length));
+
+            BoolExt result = PetProfileManager.UpdateProfilePicture(imageArr, "newTest.jpg", ImageType.jpg, 1);
+
+            Uri imageURi = new Uri("http://192.168.0.7:80");
+
+            string path = new Uri(imageURi, "/9/9/iosdf.jpg").ToString();
+
+            Console.WriteLine(result.Message);    
+        }
+    }
+}
